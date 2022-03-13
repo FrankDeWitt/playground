db128e4e56b48aa706077108763e27d1530ec982
Fran J Salvador <fs@thehotelsnetwork.com>
2022-03-13 02:16:42 +0100

    feat: basic config store

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,31 @@
+import { uuid } from '@/utils/common';
+
 export default () => ({
   isLoading: true,
-  entries: [1,2,3]
+  entries: [
+    {
+      id: uuid(),
+      date: new Date().toDateString(),
+      text: 'text1',
+      pictur: null
+    },
+    {
+      id: uuid(),
+      date: new Date().toDateString(),
+      text: 'text2',
+      pictur: null
+    },
+    {
+      id: uuid(),
+      date: new Date().toDateString(),
+      text: 'text3',
+      pictur: null
+    },
+    {
+      id: uuid(),
+      date: new Date().toDateString(),
+      text: 'text4',
+      pictur: null
+    },
+  ]
 })
